5d7869369c5161fa1209a97ead64c8c7db36f442
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-06-19 12:04:45 +0200

    feat: agregar autenticación con Redux y Axios; implementar registro y login de usuarios

---
+++ src/features/auth/authService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/users";
+
+const register = async (registerData) => {
+  const res = await axios.post(API_URL, registerData);
+  return res.data;
+};
+
+const login = async (loginData) => {
+  const res = await axios.post(API_URL, loginData);
+  if (res.data) {
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+    localStorage.setItem("token", JSON.stringify(res.data.token));
+  }
+  return res.data
+};
+
+const authService = {
+  register,
+  login,
+};
+
+export default authService;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
