0cd363081ece1f2d18f12bdf0a2c95e399bceca1
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-06-19 12:55:57 +0200

    feat: actualizar el componente Header para mostrar enlaces condicionales según el estado de autenticación del usuario

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,40 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./Header.css";
 
 const Header = () => {
+  const user = useSelector((state) => state.auth.user);
+
   return (
     <nav className="header-nav">
       <Link to="/" className="header-link">
         Home
       </Link>
-      <Link to="/card" className="header-link">
-        Card
-      </Link>
-      <Link to="/loan" className="header-link">
-        Loan
-      </Link>
-      <Link to="/profile" className="header-link">
-        Profile
-      </Link>
-      <Link to="/register" className="header-link">
-        Register
-      </Link>
-      <Link to="/login" className="header-link">
-        Login
-      </Link>
-      <Link to="/logout" className="header-link">
-        Logout
-      </Link>
+      {!user ? (
+        <>
+          <Link to="/register" className="header-link">
+            Register
+          </Link>
+          <Link to="/login" className="header-link">
+            Login
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/card" className="header-link">
+            Card
+          </Link>
+          <Link to="/loan" className="header-link">
+            Loan
+          </Link>
+          <Link to="/profile" className="header-link">
+            Profile
+          </Link>
+          <Link to="/logout" className="header-link">
+            Logout
+          </Link>
+        </>
+      )}
     </nav>
   );
 };
