333334bb9c9a4f0ebcc06b7fe3bfbd0bfcc00858
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-06-24 13:45:53 +0200

    feat: agregar componente de tarjeta con formulario para seleccionar tipo de tarjeta

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState } from "react";
+import "./Card.css";
 
 const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+  const initialState = {
+    type: "",
+  };
+  const [dataForm, setDataForm] = useState(initialState);
+  const { type } = dataForm;
 
-export default Card
+  const handleInputChange = (e) => {
+    setDataForm({ ...dataForm, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="card-container">
+      <form className="card-form">
+        <h2 className="card-title">Add Card</h2>
+        <label>
+          Card type:
+          <select
+            name="type"
+            value={type}
+            onChange={handleInputChange}
+            className="card-input"
+          >
+            <option value="">Select card type</option>
+            <option value="credit">Credit card</option>
+            <option value="debit">Debit card</option>
+          </select>
+        </label>
+        <button type="submit" className="card-btn">
+          Add card
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Card;
